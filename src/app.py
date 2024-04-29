a849113638b78faaf41e6520bc0b6e000fefb2ef
DaviMBDev <davimb00789@gmail.com>
2024-04-29 16:58:57 -0300

    Desafio 3 concluído

--- src/app.py
+++ src/app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/src/templates/componentes.html')
+def componentes():
+    return render_template('componentes.html')
+
+@app.route('/src/templates/carros.html')
+def carros():
+    return render_template('carros.html')
